d20c9a20434838a38cddb3818a65e912de5e8a18
rasit <108332998+Archessxd@users.noreply.github.com>
2023-01-15 14:33:58 +0300

    Almanca kelime tahmin etme oyunu
    Coded By Arches :)

---
+++ almanca oyunu arches/almanca oyunu arches/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace almanca_oyunu_arches
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cevap_LBL = new System.Windows.Forms.Label();
+            this.kelime_LBL = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dogu_yanlis_LBL = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.dogu_yanlis_LBL);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.kelime_LBL);
+            this.groupBox1.Controls.Add(this.cevap_LBL);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(152, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(485, 288);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Panel";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cevap";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(182, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(404, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tahmin Et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cevap_LBL
+            // 
+            this.cevap_LBL.AutoSize = true;
+            this.cevap_LBL.Location = new System.Drawing.Point(54, 264);
+            this.cevap_LBL.Name = "cevap_LBL";
+            this.cevap_LBL.Size = new System.Drawing.Size(12, 13);
+            this.cevap_LBL.TabIndex = 3;
+            this.cevap_LBL.Text = "x";
+            // 
+            // kelime_LBL
+            // 
+            this.kelime_LBL.AutoSize = true;
+            this.kelime_LBL.Location = new System.Drawing.Point(79, 44);
+            this.kelime_LBL.Name = "kelime_LBL";
+            this.kelime_LBL.Size = new System.Drawing.Size(12, 13);
+            this.kelime_LBL.TabIndex = 4;
+            this.kelime_LBL.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Kelime :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tahmin";
+            // 
+            // dogu_yanlis_LBL
+            // 
+            this.dogu_yanlis_LBL.AutoSize = true;
+            this.dogu_yanlis_LBL.Location = new System.Drawing.Point(179, 172);
+            this.dogu_yanlis_LBL.Name = "dogu_yanlis_LBL";
+            this.dogu_yanlis_LBL.Size = new System.Drawing.Size(12, 13);
+            this.dogu_yanlis_LBL.TabIndex = 7;
+            this.dogu_yanlis_LBL.Text = "x";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Cevabı Göster";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(622, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Arches Shines Like Diamonds 💎 ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Arches";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label cevap_LBL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label kelime_LBL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label dogu_yanlis_LBL;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ almanca oyunu arches/almanca oyunu arches/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Threading;
+
+namespace almanca_oyunu_arches
+{
+    public partial class Form1 : Form
+    {
+        ArrayList kelimeler = new ArrayList();
+        ArrayList anlamlar = new ArrayList();
+        string secilenkelime;
+        int indexno;
+        string secilenanlam;
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kelimeekle();
+
+            indexno = r.Next(kelimeler.Count);
+            secilenkelime = kelimeler[indexno].ToString();
+            kelime_LBL.Text = secilenkelime;
+            secilenanlam = anlamlar[indexno].ToString();
+
+        }
+        private void oyunsifirla()
+        {
+            kelimeler.Clear();
+            anlamlar.Clear();
+            kelimeekle();
+
+            indexno = r.Next(kelimeler.Count);
+            secilenkelime = kelimeler[indexno].ToString();
+            kelime_LBL.Text = secilenkelime;
+            secilenanlam = anlamlar[indexno].ToString();
+        }
+        private void kelimeekle()
+        {
+            kelimeler.Add("Hallo");
+            anlamlar.Add("merhaba");
+
+            kelimeler.Add("auf widersehen");
+            anlamlar.Add("görüşmek üzere");
+
+            kelimeler.Add("guten morgen");
+            anlamlar.Add("günaydın");
+
+            kelimeler.Add("Heute");
+            anlamlar.Add("bugün");
+
+            kelimeler.Add("Heute morgen");
+            anlamlar.Add("bu sabah");
+
+            kelimeler.Add("Gestern");
+            anlamlar.Add("dün");
+
+            kelimeler.Add("Am Mittag");
+            anlamlar.Add("öğlen");
+
+            kelimeler.Add("Nachmittag");
+            anlamlar.Add("öğleden sonra");
+
+            kelimeler.Add("Herr");
+            anlamlar.Add("bay");
+
+
+            kelimeler.Add("Frau");
+            anlamlar.Add("bayan");
+
+            kelimeler.Add("ja");
+            anlamlar.Add("evet");
+
+            kelimeler.Add("nein");
+            anlamlar.Add("hayır");
+
+            kelimeler.Add("Bitte schön");
+            anlamlar.Add("rica ederim");
+
+            kelimeler.Add("Lange");
+            anlamlar.Add("uzun");
+
+            kelimeler.Add("Mittel");
+            anlamlar.Add("orta");
+
+            kelimeler.Add("der Aschenbecher");
+            anlamlar.Add("küllük");
+
+            kelimeler.Add("die kugel");
+            anlamlar.Add("top");
+
+            kelimeler.Add("der Luftballon");
+            anlamlar.Add("balon");
+
+            kelimeler.Add("das buch");
+            anlamlar.Add("kitap");
+
+            kelimeler.Add("die flage");
+            anlamlar.Add("bayrak");
+
+            kelimeler.Add("das Feuerzeug");
+            anlamlar.Add("çakmak");
+
+        }
+       
+        private void reset() {
+            this.Controls.Clear();
+            this.InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try { 
+                string userInput = textBox1.Text;
+
+                if (userInput == secilenanlam)
+                {
+                    MessageBox.Show("doğru tahmin");
+                    reset();
+                    oyunsifirla();
+                
+                }
+                else
+                {
+                    dogu_yanlis_LBL.Text = "tekrar deneyin";
+                    textBox1.Text = null;
+                }
+                
+            }
+            catch(Exception sa)
+            {
+                MessageBox.Show(sa.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cevap_LBL.Text = anlamlar[indexno].ToString();
+        }
+    }
+}
